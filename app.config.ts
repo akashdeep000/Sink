5c7293cf8fcafed5b631059ecf27530d8f920f44
Akashdeep <76022812+akashdeep000@users.noreply.github.com>
2025-03-30 06:53:00 +0530

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -8,7 +8,7 @@ export default defineAppConfig({
   blog: 'https://sink.cool/blog',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
   image: 'https://sink.cool/banner.png',
-  previewTTL: 300, // 5 minutes
+  previewTTL: 30, // 0.5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
     'dashboard',
